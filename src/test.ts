788242317c128937d62068b2fa1624c9a8f164b0
Kapil Verma <kapv89@gmail.com>
2023-05-05 15:32:50 +0530

    avoiding broadcastchannel in test

--- src/test.ts
+++ src/test.ts
@@ -3,6 +3,14 @@ import * as Y from 'yjs';
 import {WebsocketProvider} from 'y-websocket';
 import {v4 as uuid} from 'uuid';
 
+export class NoBCWebsocketProvider extends WebsocketProvider {
+  connectBc() {
+    // making this a no-op to avoid broadcast-channel based communication
+    // b/w docs in tests
+    return;
+  }
+}
+
 const wsUrl = (port: number) => `ws://localhost:${port}`;
 
 const wait = async (seconds: number) => await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
@@ -13,8 +21,8 @@ const run = async () => {
   const doc1 = new Y.Doc();
   const doc2 = new Y.Doc();
 
-  const wsp1 = new WebsocketProvider(wsUrl(3001), id, doc1, {WebSocketPolyfill: WebSocket as any});
-  const wsp2 = new WebsocketProvider(wsUrl(3002), id, doc2, {WebSocketPolyfill: WebSocket as any});
+  const wsp1 = new NoBCWebsocketProvider(wsUrl(3001), id, doc1, {WebSocketPolyfill: WebSocket as any});
+  const wsp2 = new NoBCWebsocketProvider(wsUrl(3002), id, doc2, {WebSocketPolyfill: WebSocket as any});
 
   await wait(1);
 
