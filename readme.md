fd8473be9782815532072d445e4c88fa177e83b9
Kapil Verma <kapv89@gmail.com>
2023-05-05 15:44:05 +0530

    updated readme

--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
 # YJS horizontal scaling playground.
 
 Use this repo in front of [this repo](https://github.com/kapv89/yjs-scalable-ws-backend) to perform various operations on running yjs on top of horizontally scaled websocket-server-cluster with y-websocket
+
+# How to use
+1. clone (https://github.com/kapv89/yjs-scalable-ws-backend)
+2. `cd yjs-scalable-ws-backend`
+3. `git checkout main`
+4. `npm ci`
+5. `docker-compose up` in terminal 1
+6. `npm run tables` in terminal 2
+7. `SERVER_PORT=3001 npm run dev` in terminal 2
+8. `SERVER_PORT=3002 npm run dev` in terminal 3
+9. Open terminal 4 and `cd ..`
+10. clone (https://github.com/kapv89/yjs-scalable-ws-backend-test)
+11. `cd yjs-scalable-ws-backend-test`
+12. `npm ci`
+13. `npm run test`
--- src/test.ts
+++ src/test.ts
@@ -26,33 +26,33 @@ const run = async () => {
 
   await wait(1);
 
-  console.log('states 1 - uninitialized');
-  console.log(wsp1.awareness.getStates());
-
-  console.log('states 2 - uninitialized');
-  console.log(wsp2.awareness.getStates());
+  console.log('uninitialized');
+  console.log('states 1', wsp1.awareness.getStates());
+  console.log('states 2', wsp2.awareness.getStates());
 
   wsp1.awareness.setLocalStateField('foo', 1);
-  wsp2.awareness.setLocalStateField('foo', 2);
-
   await wait(0.3);
+  console.log('states 1 - initialized to foo:1');
+  console.log('states 1', wsp1.awareness.getStates());
+  console.log('states 2', wsp2.awareness.getStates());
 
-  console.log('states 1 - initialized');
-  console.log(wsp1.awareness.getStates());
-
-  console.log('states 2 - initialized');
-  console.log(wsp2.awareness.getStates());
+  wsp2.awareness.setLocalStateField('foo', 2);
+  await wait(0.3);
+  console.log('states 2 - initialized to foo:2');
+  console.log('states 1', wsp1.awareness.getStates());
+  console.log('states 2', wsp2.awareness.getStates());
 
   wsp1.awareness.setLocalStateField('foo', 4);
-  wsp2.awareness.setLocalStateField('foo', 3);
-
   await wait(0.3);
-  
-  console.log('states 1 - changed');
-  console.log(wsp1.awareness.getStates());
+  console.log('states 1 - changed to foo:4');
+  console.log('states 1', wsp1.awareness.getStates());
+  console.log('states 2', wsp2.awareness.getStates());
 
-  console.log('states 2 - changed');
-  console.log(wsp2.awareness.getStates());
+  wsp2.awareness.setLocalStateField('foo', 3);
+  await wait(0.3);
+  console.log('states 2 - changed to foo:3');
+  console.log('states 1', wsp1.awareness.getStates());
+  console.log('states 2', wsp2.awareness.getStates());
   
   await wait(0.3);
 
